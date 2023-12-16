0896940daca793e165c0c0adc129c393684adf85
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-12-16 18:40:11 +0500

    Add: Added new views SignUpViews

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .models import UserModel
+from .forms import SignUpForm
+
 
 # Create your views here.
+def SignupViews(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+
+            UserModel.objects.create(
+                user=user,
+                full_name=form.cleaned_data.get('full_name'),
+                phone_number=form.cleaned_data.get('phone_number')
+            )
+
+            return HttpResponse('Done!')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'register.html', {'form': form})
