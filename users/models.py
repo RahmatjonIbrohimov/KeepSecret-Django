b25e418a82609764a50be386c6d3668955541bdb
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-12-17 10:42:48 +0500

    Update: update models py

--- users/models.py
+++ users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 from django.db import models
 
 
@@ -7,7 +7,7 @@ class UserModel(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     fullname = models.CharField(max_length=30, blank=True)
     phone_number = models.CharField(max_length=15)
-    gender = models.CharField(max_length=1, choices=[('M', 'Male'), ('F', 'Female')], blank=True)
+    gender = models.CharField(max_length=6, choices=[('Male', 'Male'), ('Female', 'Female')], blank=True)
 
     def __str__(self):
         return self.user.username
