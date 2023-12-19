4bef0799758bef670211f944598d4e3bb4a833b1
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-12-19 20:50:56 +0500

    Update: updated models, added img

--- users/models.py
+++ users/models.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
 class UserModel(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    img = models.ImageField(upload_to='profile_img/', null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='usermodel')
+    img = models.ImageField(upload_to='profile_img/', default='https://www.instagram.com/p/C1AzlKiI0IW/', null=True, blank=True)
     fullname = models.CharField(max_length=30)
     phone_number = models.CharField(max_length=15)
     gender = models.CharField(max_length=6, choices=[('Male', 'Male'), ('Female', 'Female')], blank=True)
