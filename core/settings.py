09fb374dfbbf674e71168189e53d3aefb3c30349
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-12-19 22:20:57 +0500

    Update: debug mode False

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ TEMPLATES_DIR = BASE_DIR / 'templates'
 SECRET_KEY = 'django-insecure-#9g8!o6&)3a!cca&i%hvutn_k09fv^+q1(g7+_93c6pbi8ai8('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', '127.0.0.1', '.vercel.app']
 
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
